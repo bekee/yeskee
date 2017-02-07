1663376bef999b3cf289e010707e164f78beab90
Bekee Fanklin <frank@bettatechsolutions.com>
2017-02-07 03:13:14 +0100

    P65
    P65

--- resources/views/admin/level/index.blade.php
+++ resources/views/admin/level/index.blade.php
@@ -162,7 +162,7 @@
 	<script src="{{URL::asset('loggedin/assets/libs/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
 	<script src="{{URL::asset('loggedin/assets/libs/jquery-sparkline/jquery-sparkline.js')}}"></script>
 	<script src="{{URL::asset('loggedin/assets/libs/nifty-modal/js/classie.js')}}"></script>
-	<script src="{{URL::asset('loggedin/assets/libs/nifty-modal/js/modalEffects.js')}}"></script>
+	
 	<script src="{{URL::asset('loggedin/assets/libs/sortable/sortable.min.js')}}"></script>
 	<script src="{{URL::asset('loggedin/assets/libs/bootstrap-fileinput/bootstrap.file-input.js')}}"></script>
 	<script src="{{URL::asset('loggedin/assets/libs/bootstrap-select/bootstrap-select.min.js')}}"></script>
@@ -177,7 +177,5 @@
 	
 	<script src="{{URL::asset('loggedin/assets/js/init.js')}}"></script>
 	<!-- Page Specific JS Libraries -->
-	<script src="{{URL::asset('loggedin/assets/libs/jquery-easypiechart/jquery.easypiechart.min.js')}}"></script>
-	<script src="{{URL::asset('loggedin/assets/libs/jquery-knob/jquery.knob.min.js')}}"></script>
-	<script src="{{URL::asset('loggedin/assets/js/pages/other-charts.js')}}"></script>
+	
 @show
